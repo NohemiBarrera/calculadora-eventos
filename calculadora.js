36a95627887fdaba23f44c97998ba197f2eea070
“Ivan <nohemi.baol@gmail.com>
2017-03-23 16:07:17 -0600

    primera version

---
+++ calculadora.js
@@ -0,0 +1,52 @@
+var numero1 = 0;
+var numero2 = 0;
+var operacion;
+
+
+function asignarNum(numero){
+   if(document.getElementById("textNum").value == "0" || numero1 == 0){
+       numero1 = numero;
+   }else{
+       numero1 += numero;
+   }
+       document.getElementById("textNum").value = numero1;
+}
+
+function teclaC(){
+    numero1 = 0;
+    numero2 = 0;
+    document.getElementById("textNum").value = 0;
+}
+
+function operar(valor){
+    if (numero1 == 0){
+        resultado = parseInt(document.getElementById("textNum").value);
+    }
+    numero2 = parseInt(numero1);
+    numero1= 0;
+    operacion = valor;
+}
+
+
+function igual(){
+   numero1 = parseInt(numero1);
+    switch (operacion){
+        case 1:
+            numero1 += numero2;
+        break;
+        case 2:
+            numero1 = numero2 - numero1;
+        break;
+        case 3:
+            numero1 *= numero2;
+        break;
+        case 4:
+            numero1 = numero2 / numero1;
+        break;
+        case 5:
+            numero1 = Math.pow(numero2, numero1);
+        break;
+    }
+    document.getElementById("textNum").value = numero1;
+    numero2 = parseInt(numero1);
+}
